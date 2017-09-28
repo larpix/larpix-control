1e4efa3e8fdcd218e91e65da2d5d5340d55f7d99
Sam Kohn <kohn@berkeley.edu>
2017-09-28 15:06:59 -0700

    Add struct initialization and ftdi configuration methods

--- include/larpix.h
+++ include/larpix.h
@@ -1,10 +1,21 @@
 #include <ftd2xx/ftd2xx.h>
 
+typedef unsigned int uint;
+typedef unsigned char byte;
+
 typedef struct larpix_connection
 {
-    FT_HANDLE ftHandle;
-    int portNumber;
+    FT_HANDLE ft_handle;
+    int port_number;
+    uint clk_divisor;
+    byte pin_io_directions;
+    byte bit_mode;
+    byte timeout;
+    byte usb_transfer_size;
 } larpix_connection;
 
-int larpix_connect(larpix_connection c);
-int larpix_disconnect(larpix_connection c);
+void larpix_default_connection(larpix_connection* c);
+int larpix_connect(larpix_connection* c);
+int larpix_disconnect(larpix_connection* c);
+
+int larpix_init(larpix_connection* c);
--- src/larpix.c
+++ src/larpix.c
@@ -1,13 +1,38 @@
 #include "larpix.h"
 
-int larpix_connect(larpix_connection c)
+void larpix_default_connection(larpix_connection* c)
 {
-    FT_STATUS status = FT_Open(c.portNumber, &(c.ftHandle));
+    c->port_number = 0;
+    c->clk_divisor = 0;
+    c->pin_io_directions = 0x01;
+    c->bit_mode = FT_BITMODE_SYNC_BITBANG;
+    c->timeout = 10;
+    c->usb_transfer_size = 64;
+    return;
+}
+int larpix_connect(larpix_connection* c)
+{
+    FT_STATUS status = FT_Open(c.port_number, &(c->ft_handle));
     return (int) status;
 }
 
-int larpix_disconnect(larpix_connection c)
+int larpix_disconnect(larpix_connection* c)
 {
-    FT_STATUS status = FT_Close(&(c.ftHandle));
+    FT_STATUS status = FT_Close(&(c->ft_handle));
+    return (int) status;
+}
+
+int larpix_configure_ftdi(larpix_connection* c)
+{
+    FT_HANDLE* handle = &(c->ft_handle);
+    FT_STATUS status = FT_OK;
+    status | = FT_SetBitMode(handle,
+            c->pin_io_directions,
+            c->bit_mode);
+    status |= FT_SetDivisor(handle, c->clk_divisor);
+    status |= FT_SetTimeouts(handle, c->timeout, c->timeout);
+    status |= FT_SetUSBParameters(handle,
+            c->usb_transfer_size,
+            c->usb_transfer_size);
     return (int) status;
 }
