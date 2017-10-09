456c2d9b12ea984cdcf528c11de97bfb93980f12
Peter Madigan <pmadigan@10.146.3.128.in-addr.dhcp.lbl.gov>
2017-10-09 15:43:28 -0700

    Add ctypes configuration struct to python Configuration class

--- python/larpix.py
+++ python/larpix.py
@@ -34,15 +34,15 @@ class Configuration(object):
 
     '''
     def __init__(self):
-        self._pixel_trim_thresholds = [0] * 32
+        self._pixel_trim_thresholds = [0] * larpix.larpix_num_channels()
         self._global_threshold = 0
         self._csa_gain = 0
         self._csa_bypass = 0
         self._internal_bypass = 0
-        self._csa_bypass_select = [0] * 32
-        self._csa_monitor_select = [0] * 32
-        self._csa_testpulse_enable = [0] * 32
-        self._csa_testpulse_dac_amplitude = [0] * 32
+        self._csa_bypass_select = [0] * larpix.larpix_num_channels()
+        self._csa_monitor_select = [0] * larpix.larpix_num_channels()
+        self._csa_testpulse_enable = [0] * larpix.larpix_num_channels()
+        self._csa_testpulse_dac_amplitude = [0] * larpix.larpix_num_channels()
         self._test_mode = 0
         self._cross_trigger_mode = 0
         self._periodic_reset = 0
@@ -53,6 +53,41 @@ class Configuration(object):
         self._channel_mask = 0
         self._external_trigger_mask = 0
         self._reset_cycles = 0
+        
+        self._configuration = self.get_ctypes_configuration() 
+
+    def get_ctypes_configuration(self):
+        return_config = l.larpix_configuration()
+
+        for i,value in enumerate(self._pixel_trim_thresholds):
+            return_config.pixel_trim_thresholds[i] = value
+        return_config.global_threshold = self._global_threshold
+        return_config.csa_gain = self._csa_gain
+        return_config.csa_bypass = self._csa_bypass
+        return_config.internal_bypass = self._internal_bypass
+        for i,value in enumerate(self._csa_bypass_select):
+            return_config.csa_bypass_select[i] = value
+        for i,value in enumerate(self._csa_monitor_select):
+            return_config.csa_monitor_select[i] = value
+        for i,value in enumerate(self._csa_testpulse_enable):
+            return_config.csa_testpulse_enable[i] = value
+        return_config.csa_testpulse_dac_amplitude = self._csa_testpulse_dac_amplitude
+        return_config.test_mode = self._test_mode
+        return_config.cross_trigger_mode = self._cross_trigger_mode
+        return_config.periodic_reset = self._periodic_reset
+        return_config.fifo_diagnostic = self._fifo_diagnostic
+        return_config.sample_cycles = self._sample_cycles
+        for i,value in enumerate(self._test_burst_length):
+            return_config.test_burst_length[i] = value
+        return_config.adc_burst_length = self._adc_burst_length
+        for i,value in enumerate(self._channel_mask):
+            return_config.channel_mask[i] = value
+        for i,value in enumerate(self._external_trigger_mask):
+            return_config.external_trigger_mask[i] = value
+        for i,value in enumerate(self._reset_cycles):
+            return_config.reset_cycles[i] = value
+        
+        return return_config
 
     def get_uarts(self):
         pass
