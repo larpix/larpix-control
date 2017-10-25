larpix/larpix-control

commit 6405e15bc7801c3623ecf1c9dc062cf3f2922684

Add a class variable to tell which bits to use to calculate parity

larpix/larpix.py
@@ -385,6 +385,7 @@ class Packet(object):
     packet_type_bits = slice(52, 54)
     chipid_bits = slice(44, 52)
     parity_bit = 0
+    parity_calc_bits = slice(1, 53)
     channel_id_bits = slice(37, 44)
     timestamp_bits = slice(13, 37)
     dataword_bits = slice(3, 13)
@@ -460,7 +461,7 @@ class Packet(object):
         self.bits[Packet.parity_bit] = value
 
     def compute_parity(self):
-        return 1 - (self.bits[1:].count(True) % 2)
+        return 1 - (self.bits[Packet.parity_calc_bits].count(True) % 2)
 
     def assign_parity(self):
         self.parity_bit_value = self.compute_parity()

commit 4e3306f21521adccb1e2dd2d2e9942a33a2d9fc3

Fix bug in calculation of parity introduced in 6405e15

larpix/larpix.py
@@ -762,7 +762,7 @@ class Packet(object):
     packet_type_bits = slice(52, 54)
     chipid_bits = slice(44, 52)
     parity_bit = 0
-    parity_calc_bits = slice(1, 53)
+    parity_calc_bits = slice(1, 54)
     channel_id_bits = slice(37, 44)
     timestamp_bits = slice(13, 37)
     dataword_bits = slice(3, 13)

test/test_larpix.py
@@ -166,6 +166,14 @@ def test_packet_compute_parity():
     parity = p.compute_parity()
     expected = 0
     assert parity == expected
+    p.bits = BitArray([0]*54)
+    parity = p.compute_parity()
+    expected = 1
+    assert parity == expected
+    p.bits = BitArray([1]*54)
+    parity = p.compute_parity()
+    expected = 0
+    assert parity == expected
 
 def test_packet_assign_parity():
     p = Packet()
@@ -177,6 +185,24 @@ def test_packet_assign_parity():
     p.assign_parity()
     expected = 1
     assert p.parity_bit_value == expected
+
+def test_packet_has_valid_parity():
+    p = Packet()
+    result = p.has_valid_parity()
+    expected = False
+    assert result == expected
+    p.assign_parity()
+    result = p.has_valid_parity()
+    expected = True
+    assert result == expected
+    p.bits = BitArray([1]*54)
+    result = p.has_valid_parity()
+    expected = False
+    assert result == expected
+    p.assign_parity()
+    result = p.has_valid_parity()
+    expected = True
+    assert result == expected
 
 def test_packet_set_channel_id():
     p = Packet()