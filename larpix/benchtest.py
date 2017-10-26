5be58dcfa1c1d6cf54fe7a34295045813c005da9
Sam Kohn <kohn@berkeley.edu>
2017-10-26 16:23:33 -0700

    Add benchtest for "read register test". Also add basic error handling

--- larpix/benchtest.py
+++ larpix/benchtest.py
@@ -60,10 +60,58 @@ def io_loopback_test(settings):
     bytestream = [controller.format_UART(chip, packet)]
     data = controller.serial_write_read(bytestream, 1)
     controller.parse_input(data)
-    returned_packet_str = str(chip.reads[0])
-    logger.info(' - Received packet %s', returned_packet_str)
+    if len(chip.reads) > 0:
+        returned_packet_str = str(chip.reads[0])
+        logger.info(' - Received packet %s', returned_packet_str)
+    else:
+        logger.warning(' - Did not receive any packets')
+
+
+def read_register_test(settings):
+    '''
+    Send "config read" packet and read back the configuration.
+
+    Verify that the packet returns to the control system with the
+    expected data. Repeat for every register on every chip.
+
+    '''
+    logger = logging.getLogger(__name__)
+    logger.info('Performing io_loopback_test')
+    port = settings['port']
+    controller = larpix.Controller(port)
+    chipset = settings['chipset']
+    for chipargs in chipset:
+        chip = larpix.Chip(*chipargs)
+        controller.chips.append(chip)
+    for chip in controller.chips:
+        remainder = controller.read_configuration(chip)
+        if remainder != b'':
+            logger.warning(' - %s returned invalid bytes: %s',
+                    str(chip), str(remainder))
+        # The chip configuration should be default, so the config we
+        # have recorded in software is the same as the expected
+        # configuration of the actual chips
+        expected_packets = chip.get_configuration_packets(
+                larpix.Packet.CONFIG_READ_PACKET)
+        actual_packets = chip.reads
+        missing_packets = [p for p in expected_packets if p not in
+                actual_packets]
+        extra_packets = [p for p in actual_packets if p not in
+                expected_packets]
+        if missing_packets:
+            logger.warning(' - %s is missing packets: \n    %s',
+                    str(chip), '\n    '.join(map(str, missing_packets)))
+        if extra_packets:
+            logger.warning(' - %s has extra packets: \n    %s',
+                    str(chip), '\n    '.join(map(str, extra_packets)))
+
 
 if __name__ == '__main__':
     setup_logger({})
-    pcb_io_test({'port':'/dev/ttyUSB1'})
-    io_loopback_test({'port':'/dev/ttyUSB1', 'chipid':1,'io_chain':0})
+    logger = logging.getLogger(__name__)
+    try:
+        pcb_io_test({'port':'/dev/ttyUSB1'})
+        io_loopback_test({'port':'/dev/ttyUSB1', 'chipid':1,'io_chain':0})
+        read_register_test({'port':'/dev/ttyUSB1', 'chipset':[(1,0)]})
+    except Exception as e:
+        logger.error('Error during test', exc_info=True)
