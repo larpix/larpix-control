2d9eb3ef92d16e755794481e9b5b13c9e94a74bc
Sam Kohn <kohn@berkeley.edu>
2017-11-01 11:38:28 -0700

    Bump setup.py version number

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
         name='larpix-control',
-        version='0.4.1.dev2',
+        version='0.4.1.dev3',
         description='Control the LArPix chip',
         long_description=long_description,
         url='https://github.com/samkohn/larpix-control',
