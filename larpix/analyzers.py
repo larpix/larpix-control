51990f514854c78441efaf8f101b8ebf4ff01b96
Daniel Dwyer <dadwyer@lbl.gov>
2017-12-11 11:10:42 -0800

    Add tool to report rate of packet parity problems.

--- larpix/analyzers.py
+++ larpix/analyzers.py
@@ -28,6 +28,51 @@ class LogAnalyzer(DataLoader):
                 break
             self.print_transmission(next_trans)
         return
+
+    def check_parity(self, transmission):
+        '''Check parities of all packets in transmission'''
+        npackets_bad_parity = 0
+        if transmission['block_type'] != 'data': return 0
+        return sum(1 for pack in transmission['packets']
+                   if not pack.has_valid_parity())
+
+
+    def parity_report(self, interval_step=100000):
+        '''Check parities of all packets in log, and report status'''
+        print('========= Parity Report =====================')
+        npackets_total = 0
+        npackets_bad_parity_total = 0
+        npackets_interval = 0
+        npackets_bad_parity_interval = 0
+        while True:
+            next_trans = self.next_transmission()
+            if next_trans is None:
+                break
+            if next_trans['block_type'] != 'data': continue
+            if next_trans['data_type'] != 'read': continue
+            if len(next_trans['packets']) <= 0: continue
+            npackets_bad_parity = self.check_parity(next_trans)
+            npackets_total += len(next_trans['packets'])
+            npackets_bad_parity_total += npackets_bad_parity
+            npackets_interval += len(next_trans['packets'])
+            npackets_bad_parity_interval += npackets_bad_parity
+            if npackets_interval > interval_step:
+                # Interval Report
+                print('  N_packets, N_badparity, Bad Fraction: %d %d %f' % (
+                    npackets_interval,
+                    npackets_bad_parity_interval,
+                    npackets_bad_parity_interval/float(npackets_interval)))
+                npackets_interval = 0
+                npackets_bad_parity_interval = 0
+        # Total Report
+        print('== Summary of Parity Check ==')
+        print('  Total number of packets: %d' % npackets_total)
+        print('  Packets with bad parity: %d' % npackets_bad_parity_total)
+        if npackets_total > 0:
+            print('                 Fraction: %f' % (npackets_bad_parity_total/
+                                                     float(npackets_total)))
+        return
+
     
     @classmethod
     def print_transmission(cls, block, show_packets=True):
