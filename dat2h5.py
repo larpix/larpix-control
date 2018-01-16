f93c624b93217846c2fe586ecc55f9070895fc53
Sam Kohn <kohn@berkeley.edu>
2018-01-16 11:49:48 -0800

    Add ROOT support to dat2h5.py

--- dat2h5.py
+++ dat2h5.py
@@ -1,6 +1,6 @@
 '''
-A script to convert a .dat data file into a simple h5 file with the
-following columns:
+A script to convert a .dat data file into the specified format with the
+following data:
 
     channel id | chip id | pixel id | pixel x | pixel y | raw ADC | raw
     timestamp | 6-bit ADC | full timestamp
@@ -8,9 +8,8 @@ following columns:
 '''
 
 from __future__ import print_function
-import numpy as np
-import h5py
 import argparse
+import numpy as np
 from larpix.dataloader import DataLoader
 from larpix.larpix import Controller
 from larpixgeometry.pixelplane import PixelPlane
@@ -29,12 +28,39 @@ parser = argparse.ArgumentParser()
 parser.add_argument('infile')
 parser.add_argument('outfile')
 parser.add_argument('-v', '--verbose', action='store_true')
+parser.add_argument('--format', choices=['h5', 'root', 'ROOT']
 args = parser.parse_args()
 
 infile = args.infile
 outfile = args.outfile
 verbose = args.verbose
 loader = DataLoader(infile)
+
+if args.format == 'h5':
+    import h5py
+elif args.format.lower() == 'root':
+    use_root = True
+    import ROOT
+    root_channelid = np.array([-1], dtype=int)
+    root_chipid = np.array([-1], dtype=int)
+    root_pixelid = np.array([-1], dtype=int)
+    root_pixelx = np.array([0], dtype=float)
+    root_pixely = np.array([0], dtype=float)
+    root_rawADC = np.array([-1], dtype=int)
+    root_rawTimestamp = np.array([0], dtype=np.uint64)
+    root_ADC = np.array([-1], dtype=int)
+    root_timestamp = np.array([0], dtype=np.uint64)
+    fout = ROOT.TFile(outfile, 'recreate')
+    ttree = ROOT.TTree('larpixdata', 'LArPixData')
+    ttree.Branch('channelid', root_channelid, 'channelid/I')
+    ttree.Branch('chipid', root_chipid, 'chipid/I')
+    ttree.Branch('pixelid', root_pixelid, 'pixelid/I')
+    ttree.Branch('pixelx', root_pixelx, 'pixelx/D')
+    ttree.Branch('pixely', root_pixely, 'pixely/D')
+    ttree.Branch('raw_adc', root_raw_adc, 'raw_adc/I')
+    ttree.Branch('raw_timestamp', root_raw_timestamp, 'raw_timestamp/l')
+    ttree.Branch('adc', root_adc, 'adc/I')
+    ttree.Branch('timestamp', root_timestamp, 'timestamp/l')
 
 geometry = PixelPlane.fromDict(layouts.load('sensor_plane_28_simple.yaml'))
 
@@ -55,6 +81,7 @@ while True:
                 current_array[current_index][5] = packet.dataword
                 current_array[current_index][6] = packet.timestamp
                 current_array[current_index][7] = fix_ADC(packet.dataword)
+                current_array[current_index][8] = 0
 
                 chipid = packet.chipid
                 channel = packet.channel_id
@@ -62,18 +89,31 @@ while True:
                 current_array[current_index][2] = pixel.pixelid
                 current_array[current_index][3] = pixel.x
                 current_array[current_index][4] = pixel.y
-                current_index += 1
-                if current_index == index_limit:
-                    current_index = 0
-                    numpy_arrays.append(np.empty((index_limit, 9),
-                        dtype=np.float64))
-                    current_array = numpy_arrays[-1]
+                if use_root:
+                    (root_channelid[0], root_chipid[0], root_pixelid[0],
+                            root_pixelx[0], root_pixely[0],
+                            root_rawADC[0], root_rawTimestamp[0],
+                            root_ADC[0], root_timestamp[0]
+                        ) = current_array[current_index]
+                    ttree.Fill()
+                else:
+                    current_index += 1
+                    if current_index == index_limit:
+                        current_index = 0
+                        numpy_arrays.append(np.empty((index_limit, 9),
+                            dtype=np.float64))
+                        current_array = numpy_arrays[-1]
 
-numpy_arrays[-1] = numpy_arrays[-1][:current_index]
-final_array = np.vstack(numpy_arrays)
-with h5py.File(outfile, 'w') as outfile:
-    dset = outfile.create_dataset('data', data=final_array)
-    dset.attrs['descripiton'] = '''
-channel id | chip id | pixel id | pixel x | pixel y | raw ADC | raw
-timestamp | 6-bit ADC | full timestamp'''
+if use_root:
+    ttree.Write()
+    fout.Write()
+    fout.Close()
+else:
+    numpy_arrays[-1] = numpy_arrays[-1][:current_index]
+    final_array = np.vstack(numpy_arrays)
+    with h5py.File(outfile, 'w') as outfile:
+        dset = outfile.create_dataset('data', data=final_array)
+        dset.attrs['descripiton'] = '''
+    channel id | chip id | pixel id | pixel x | pixel y | raw ADC | raw
+    timestamp | 6-bit ADC | full timestamp'''
 
