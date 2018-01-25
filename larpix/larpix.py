591e22c5721df181f4506ca0d3a14c156d38975b
Peter Madigan <pmadigan@berkeley.edu>
2018-01-25 15:45:38 -0800

    Fix non_default_registers pytests

--- larpix/larpix.py
+++ larpix/larpix.py
@@ -214,14 +214,14 @@ class Configuration(object):
                 d[register_name] = (getattr(self, register_name), getattr(config,
                                                                           register_name))
         # Attempt to simplify some of the long values (array values)
-        for (name, value) in d.items():
+        for (name, (self_value, config_value)) in d.items():
             if (name in (label for _, label in self._complex_array_spec)
                     or name == 'pixel_trim_thresholds'):
                 different_values = []
-                for ch, (val, config_val) in enumerate(zip(value, getattr(
-                    config, name))):
+                for ch, (val, config_val) in enumerate(zip(self_value, config_value)):
                     if val != config_val:
-                        different_values.append({'channel': ch, 'value': val})
+                        different_values.append(({'channel': ch, 'value': val},
+                                                 {'channel': ch, 'value': config_val}))
                 if len(different_values) < 5:
                     d[name] = different_values
                 else:
