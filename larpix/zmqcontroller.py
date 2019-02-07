07effa591da8a4dba017e3a6426b3500b2e64ebc
Sam Kohn <kohn@berkeley.edu>
2019-02-06 20:00:08 -0800

    Update zmqcontroller to new IO scheme

--- larpix/zmqcontroller.py
+++ larpix/zmqcontroller.py
@@ -1,43 +1,45 @@
 import time
 import zmq
+from .larpix import Packet
 
-class Serial_ZMQ(object):
-    def __init__(self, port, timeout):
-        self.port = port
-        self.timeout = timeout
+class ZMQ_IO(object):
+    def __init__(self, address):
         self.context = zmq.Context()
-        self.socket = self.context.socket(zmq.REQ)
-        self.socket.bind(self.port)
+        self.sender = self.context.socket(zmq.REQ)
+        self.receiver = self.context.socket(zmq.SUB)
+        send_address = address + ':5555'
+        receive_address = address + ':5556'
+        self.sender.connect(send_address)
+        self.receiver.connect(receive_address)
+        self.sender_replies = []
+        self.is_listening = False
+        self.poller = zmq.Poller()
+        self.poller.register(self.receiver, zmq.POLLIN)
 
-    def open(self):
-        pass
+    def send(self, packets):
+        self.sender_replies = []
+        for packet in packets:
+            tosend = b'SNDWORD 0x00%s 0' % packet.bytes()[::-1].hex().encode()
+            self.sender.send(tosend)
+            self.sender_replies.append(self.sender.recv())
 
-    def close(self):
-        pass
+    def start_listening(self):
+        if self.is_listening:
+            raise RuntimeError('Already listening')
+        self.receiver.setsockopt(zmq.SUBSCRIBE, b'')
 
-    def write(self, bytestream):
-        '''Send the bytestream.'''
-        self.socket.send_multipart([b'0', bytestream])
-        self.socket.recv()
+    def stop_listening(self):
+        if not self.is_listening:
+            raise RuntimeError('Already not listening')
+        self.receiver.setsockopt(zmq.UNSUBSCRIBE, b'')
 
-    def read(self, timeout=None):
-        '''Read for the given duration, in seconds.
+    def empty_queue(self):
+        packets = []
+        while self.poller.poll(0):
+            message = self.receiver.recv()
+            if len(m) % 8 == 0:
+                for start_index in range(0, len(m), 8):
+                    packet_bytes = message[start_index:start_index+7]
+                    packets.append(Packet(packet_bytes))
+        return packets
 
-           If ``timeout is None``, use ``self.timeout``.
-        '''
-        timeout = timeout or self.timeout
-        self.socket.send_string(str(timeout), flags=zmq.SNDMORE)
-        self.socket.send(b'')
-        bytestream = self.socket.recv()
-        return bytestream
-
-    def write_read(self, bytestream, timeout=None):
-        '''Write the given bytestream, then read for the given duration.
-
-           If ``timeout is None``, use ``self.timeout``.
-        '''
-        timeout = timeout or self.timeout
-        self.socket.send_string(str(timeout), flags=zmq.SNDMORE)
-        self.socket.send(bytestream)
-        new_bytestream = self.socket.recv()
-        return new_bytestream
