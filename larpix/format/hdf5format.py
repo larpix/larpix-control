fd5dd7529706c7719ff3a5862f7518f72f1f8746
Sam Kohn <kohn@berkeley.edu>
2019-06-20 14:24:16 -0700

    Update HDF5 format to v1.0; add tests & documentation
    Added extra data attribute to the hdf5format module to help record the
    list index of each packet attribute since that could change between
    versions.
    
    Added version checking in to_file and from_file, including "compatible
    with" semantics so that a script expecting ~1.5 can read a file saved in
    1.6.
    
    Added tests for both v0.0 and v1.0.

--- larpix/format/hdf5format.py
+++ larpix/format/hdf5format.py
@@ -7,9 +7,6 @@ File format description
 All LArPix+HDF5 files use the HDF5 format so that they
 can be read and written using any language that has an HDF5 binding.
 
-LArPix+HDF5 files are self-describing in that they contain a format
-version which identifies the structure of the file.
-
 File Header
 -----------
 
@@ -18,7 +15,33 @@ minimum, the header will contain the following HDF5 attributes:
 
     - ``version``: a string containing the LArPix+HDF5 version
     - ``created``: a Unix timestamp of the file's creation time
-    - ``modified``: a Unix timestamp of the files last-modified time
+    - ``modified``: a Unix timestamp of the file's last-modified time
+
+Versions
+--------
+
+The LArPix+HDF5 format is self-describing and versioned. This means as
+the format evolves, the files themselves will identify which version
+of the format should be used to interpret them. When writing a file
+with ``to_file``, the format version can be specified, or by default,
+the latest version is used. When reading a file with ``from_file``, by
+default, the format version of the actual file is used. If a specific
+format version is expected or required, that version can be specified,
+and a ``RuntimeError`` will be raised if a different format version is
+encountered.
+
+The versions are always in the format ``major.minor`` and are stored as
+strings (e.g. ``'1.0'``, ``'1.5'``).
+
+The minor format will increase if a non-breaking change is made, so that
+a script compatible with a lower minor version will also work with files
+that have a higher minor version. E.g. a script designed to work with
+v1.0 will also work with v1.5. The reverse is not necessarily true: a
+script designed to work with v1.5 *may not* work with v1.0 files.
+
+The major format will increase if a breaking change is made. This means
+that a script designed to work with v1.5 will *likely not* work with
+v2.0 files, and vice versa.
 
 File Data
 ---------
@@ -26,7 +49,7 @@ File Data
 The file data is saved in HDF5 datasets, and the specific data format
 depends on the LArPix+HDF5 version.
 
-For version 0.0, there is only one dataset: ``raw_packet``. This dataset
+For version 1.0, there is only one dataset: ``packets``. This dataset
 contains a list of all of the packets sent and received during a
 particular time interval.
 
@@ -85,10 +108,32 @@ import numpy as np
 
 from larpix.larpix import Packet, TimestampPacket
 
-# {version: {dset_name: [structured dtype fields]}}
+#: The most recent / up-to-date LArPix+HDF5 format version
+latest_version = '1.0'
+
+#: The dtype specification used in the HDF5 files.
+#:
+#: Structure: ``{version: {dset_name: [structured dtype fields]}}``
 dtypes = {
         '0.0': {
             'raw_packet': [
+                ('chip_key','S32'),
+                ('type','u1'),
+                ('chipid','u1'),
+                ('parity','u1'),
+                ('valid_parity','u1'),
+                ('counter','u4'),
+                ('channel','u1'),
+                ('timestamp','u8'),
+                ('adc_counts','u1'),
+                ('fifo_half','u1'),
+                ('fifo_full','u1'),
+                ('register','u1'),
+                ('value','u1'),
+                ]
+            },
+        '1.0': {
+            'packets': [
                 ('chip_key','S32'),
                 ('type','u1'),
                 ('chipid','u1'),
@@ -107,7 +152,49 @@ dtypes = {
             }
         }
 
-def to_file(filename, packet_list, mode='a', version='0.0'):
+#: A map between attribute name and "column index" in the structured
+#: dtypes.
+#:
+#: Structure: ``{version: {dset_name: {field_name: index}}}``
+dtype_property_index_lookup = {
+        '0.0': {
+            'raw_packet': {
+                'chip_key': 0,
+                'type': 1,
+                'chipid': 2,
+                'parity': 3,
+                'valid_parity': 4,
+                'counter': 5,
+                'channel': 6,
+                'timestamp': 7,
+                'adc_counts': 8,
+                'fifo_half': 9,
+                'fifo_full': 10,
+                'register': 11,
+                'value': 12,
+                }
+            },
+        '1.0': {
+            'packets': {
+                'chip_key': 0,
+                'type': 1,
+                'chipid': 2,
+                'parity': 3,
+                'valid_parity': 4,
+                'channel': 5,
+                'timestamp': 6,
+                'adc_counts': 7,
+                'fifo_half': 8,
+                'fifo_full': 9,
+                'register': 10,
+                'value': 11,
+                'counter': 12,
+                'direction': 13,
+                }
+            }
+        }
+
+def to_file(filename, packet_list, mode='a', version=None):
     '''
     Save the given packets to the given file.
 
@@ -118,24 +205,36 @@ def to_file(filename, packet_list, mode='a', version='0.0'):
         ``TimestampPacket``.
     :param mode: optional, the "file mode" to open the data file
         (default: ``'a'``)
-    :param version: optional, the LArPix+HDF5 format version to use (for
-        backwards compatibility pick an earlier version) (default: '0.0')
+    :param version: optional, the LArPix+HDF5 format version to use. If
+        writing a new file and version is unspecified or ``None``,
+        the latest version will be used. If writing an existing file
+        and version is unspecified or ``None``, the existing file's
+        version will be used. If writing an existing file and version
+        is specified and does not exactly match the existing file's
+        version, a ``RuntimeError`` will be raised. (default: ``None``)
 
     '''
     with h5py.File(filename, mode) as f:
         # Create header
         if '_header' not in f.keys():
+            if version is None:
+                version = latest_version
             header = f.create_group('_header')
             header.attrs['version'] = version
             header.attrs['created'] = time.time()
         else:
             header = f['_header']
+            file_version = header.attrs['version']
+            if header.attrs['version'] != version:
+                raise RuntimeError('Incompatible versions: existing: %s, '
+                    'specified: %s' % (file_version, version))
         header.attrs['modified'] = time.time()
 
         # Create dataset
-        dtype = dtypes[version]['raw_packet']
-        if 'raw_packet' not in f.keys():
-            dset = f.create_dataset('raw_packet', shape=(len(packet_list),),
+        dset_name = next(iter(dtypes[version]))  # assumes only 1 dset
+        dtype = dtypes[version][dset_name]
+        if dset_name not in f.keys():
+            dset = f.create_dataset(dset_name, shape=(len(packet_list),),
                     maxshape=(None,), dtype=dtype)
             dset.attrs['packet_types'] = '''
 0: 'data',
@@ -146,7 +245,7 @@ def to_file(filename, packet_list, mode='a', version='0.0'):
 '''
             start_index = 0
         else:
-            dset = f['raw_packet']
+            dset = f[dset_name]
             start_index = dset.shape[0]
             dset.resize(start_index + len(packet_list), axis=0)
 
@@ -159,43 +258,73 @@ def to_file(filename, packet_list, mode='a', version='0.0'):
                     else dict_rep.get(key, 0) for key, val_type in dtype)))
         dset[start_index:] = encoded_packets
 
-def from_file(filename):
+def from_file(filename, version=None):
     '''
     Read the data from the given file into LArPix Packet objects.
 
     :param filename: the name of the file to read
+    :param version: the format version. Specify this parameter to
+        enforce a version check. When a specific version such as
+        ``'1.5'`` is specified, a ``RuntimeError`` will be raised if the
+        stored format version number is not an exact match. If a version
+        is prefixed with ``'~'`` such as ``'~1.5'``, a ``RuntimeError``
+        will be raised if the stored format version is *incompatible*
+        with the specified version. Compatible versions are those with
+        the same major version and at least the same minor version. E.g.
+        for ``'~1.5'``, versions between v1.5 and v2.0 are compatible.
+        If unspecified or ``None``, will use the stored format version.
     :returns packet_dict: a dict with keys ``'packets'`` containing a
         list of packet objects; and ``'created'``, ``'modified'``, and
         ``'version'``, containing the file metadata.
 
     '''
     with h5py.File(filename, 'r') as f:
-        if f['_header'].attrs['version'] != '0.0':
-            raise RuntimeError('Invalid version (should be 0.0): %s' %
-                    f['_header'].attrs['version'])
+        file_version = f['_header'].attrs['version']
+        if version is None:
+            version = file_version
+        elif version[0] == '~':
+            file_major, _, file_minor = file_version.split('.')
+            version_major, _, version_minor = version.split('.')
+            version_major = version_major[1:]
+            if (file_major != version_major
+                    or file_minor < version_minor):
+                raise RuntimeError('Incompatible versions: existing: %s, '
+                    'specified: %s' % (file_version, version))
+            else:
+                version = file_version
+        elif version == file_version:
+            pass
+        else:
+            raise RuntimeError('Incompatible versions: existing: %s, '
+                'specified: %s' % (file_version, version))
+        if version not in dtypes:
+            raise RuntimeError('Unknown version: %s' % version)
+        dset_name = next(iter(dtypes[version]))  # assumes only 1 dset
+        props = dtype_property_index_lookup[version][dset_name]
         packets = []
-        for row in f['raw_packet']:
+        for row in f[dset_name]:
             if row[1] == 4:
-                packets.append(TimestampPacket(row[6]))
+                packets.append(TimestampPacket(row[props['timestamp']]))
                 continue
             p = Packet()
-            p.chip_key = row[0]
-            p.packet_type = row[1]
-            p.chipid = row[2]
-            p.parity_bit_value = row[3]
+            p.chip_key = row[props['chip_key']]
+            p.packet_type = row[props['type']]
+            p.chipid = row[props['chipid']]
+            p.parity_bit_value = row[props['parity']]
             if p.packet_type == Packet.DATA_PACKET:
-                p.channel = row[5]
-                p.timestamp = row[6]
-                p.dataword = row[7]
-                p.fifo_half_flag = row[8]
-                p.fifo_full_flag = row[9]
+                p.channel = row[props['channel']]
+                p.timestamp = row[props['timestamp']]
+                p.dataword = row[props['adc_counts']]
+                p.fifo_half_flag = row[props['fifo_half']]
+                p.fifo_full_flag = row[props['fifo_full']]
             elif p.packet_type == Packet.TEST_PACKET:
-                p.counter = row[12]
+                p.counter = row[props['counter']]
             elif (p.packet_type == Packet.CONFIG_WRITE_PACKET
                     or p.packet_type == Packet.CONFIG_READ_PACKET):
-                p.register_address = row[10]
-                p.register_data = row[11]
-            p.direction = row[13]
+                p.register_address = row[props['register']]
+                p.register_data = row[props['value']]
+            if version != '0.0':
+                p.direction = row[props['direction']]
             packets.append(p)
         return {
                 'packets': packets,
--- test/test_hdf5format.py
+++ test/test_hdf5format.py
@@ -4,7 +4,8 @@ import pytest
 import h5py
 
 from larpix.larpix import (Packet, PacketCollection, TimestampPacket)
-from larpix.format.hdf5format import to_file, from_file
+from larpix.format.hdf5format import (to_file, from_file,
+        dtype_property_index_lookup)
 
 @pytest.fixture
 def tmpfile(tmpdir):
@@ -21,6 +22,7 @@ def data_packet():
     p.fifo_half_flag = 1
     p.assign_parity()
     p.chip_key = 'hello'
+    p.direction = 1
     return p
 
 @pytest.fixture
@@ -32,14 +34,16 @@ def config_read_packet():
     p.register_data = 23
     p.assign_parity()
     p.chip_key = 'hello'
+    p.direction = 1
     return p
 
 @pytest.fixture
 def timestamp_packet():
-    return TimestampPacket(timestamp=12345)
+    p = TimestampPacket(timestamp=12345)
+    return p
 
-def test_to_file_empty(tmpfile):
-    to_file(tmpfile, [])
+def test_to_file_v0_0_empty(tmpfile):
+    to_file(tmpfile, [], version='0.0')
     f = h5py.File(tmpfile, 'r')
     assert f['_header']
     assert f['_header'].attrs['version']
@@ -47,57 +51,136 @@ def test_to_file_empty(tmpfile):
     assert f['_header'].attrs['modified']
     assert f['raw_packet']
 
-def test_to_file_data_packet(tmpfile, data_packet):
-    to_file(tmpfile, [data_packet])
+def test_to_file_v0_0_data_packet(tmpfile, data_packet):
+    to_file(tmpfile, [data_packet], version='0.0')
     f = h5py.File(tmpfile, 'r')
     assert len(f['raw_packet']) == 1
     row = f['raw_packet'][0]
+    props = dtype_property_index_lookup['0.0']['raw_packet']
     new_packet = Packet()
-    new_packet.chip_key = row[0]
-    new_packet.packet_type = row[1]
-    new_packet.chipid = row[2]
-    new_packet.parity_bit_value = row[3]
-    new_packet.channel = row[5]
-    new_packet.timestamp = row[6]
-    new_packet.dataword = row[7]
-    new_packet.fifo_half_flag = row[8]
-    new_packet.fifo_full_flag = row[9]
+    new_packet.chip_key = row[props['chip_key']]
+    new_packet.packet_type = row[props['type']]
+    new_packet.chipid = row[props['chipid']]
+    new_packet.parity_bit_value = row[props['parity']]
+    new_packet.channel = row[props['channel']]
+    new_packet.timestamp = row[props['timestamp']]
+    new_packet.dataword = row[props['adc_counts']]
+    new_packet.fifo_half_flag = row[props['fifo_half']]
+    new_packet.fifo_full_flag = row[props['fifo_full']]
     assert new_packet == data_packet
 
-def test_to_file_config_read_packet(tmpfile, config_read_packet):
-    to_file(tmpfile, [config_read_packet])
+def test_to_file_v0_0_config_read_packet(tmpfile, config_read_packet):
+    to_file(tmpfile, [config_read_packet], version='0.0')
     f = h5py.File(tmpfile, 'r')
     assert len(f['raw_packet']) == 1
     row = f['raw_packet'][0]
+    props = dtype_property_index_lookup['0.0']['raw_packet']
     new_packet = Packet()
-    new_packet.chip_key = row[0]
-    new_packet.packet_type = row[1]
-    new_packet.chipid = row[2]
-    new_packet.parity_bit_value = row[3]
-    new_packet.register_address = row[10]
-    new_packet.register_data = row[11]
+    new_packet.chip_key = row[props['chip_key']]
+    new_packet.packet_type = row[props['type']]
+    new_packet.chipid = row[props['chipid']]
+    new_packet.parity_bit_value = row[props['parity']]
+    new_packet.register_address = row[props['register']]
+    new_packet.register_data = row[props['value']]
     assert new_packet == config_read_packet
 
-def test_to_file_timestamp_packet(tmpfile, timestamp_packet):
-    to_file(tmpfile, [timestamp_packet])
+def test_to_file_v0_0_timestamp_packet(tmpfile, timestamp_packet):
+    to_file(tmpfile, [timestamp_packet], version='0.0')
     f = h5py.File(tmpfile, 'r')
     assert len(f['raw_packet']) == 1
     row = f['raw_packet'][0]
-    assert row[1] == 4  # packet_type
+    props = dtype_property_index_lookup['0.0']['raw_packet']
+    assert row[props['type']] == 4  # packet_type
     new_packet = TimestampPacket()
-    new_packet.timestamp = row[6]
+    new_packet.timestamp = row[props['timestamp']]
     assert new_packet == timestamp_packet
 
-def test_to_file_many_packets(tmpfile, data_packet, config_read_packet,
+def test_to_file_v0_0_many_packets(tmpfile, data_packet, config_read_packet,
         timestamp_packet):
-    to_file(tmpfile, [data_packet, config_read_packet, timestamp_packet])
+    to_file(tmpfile, [data_packet, config_read_packet,
+        timestamp_packet], version='0.0')
     f = h5py.File(tmpfile, 'r')
     assert len(f['raw_packet']) == 3
 
-def test_from_file_many_packets(tmpfile, data_packet,
+def test_from_file_v0_0_many_packets(tmpfile, data_packet,
         config_read_packet, timestamp_packet):
     packets = [data_packet, config_read_packet, timestamp_packet]
-    to_file(tmpfile, packets)
+    to_file(tmpfile, packets, version='0.0')
+    new_packets_dict = from_file(tmpfile, version='0.0')
+    assert new_packets_dict['created']
+    assert new_packets_dict['version']
+    assert new_packets_dict['modified']
+    new_packets = new_packets_dict['packets']
+    assert new_packets[0] == data_packet
+    assert new_packets[1] == config_read_packet
+    assert new_packets[2] == timestamp_packet
+
+def test_to_file_v1_0_empty(tmpfile):
+    to_file(tmpfile, [], version='1.0')
+    f = h5py.File(tmpfile, 'r')
+    assert f['_header']
+    assert f['_header'].attrs['version']
+    assert f['_header'].attrs['created']
+    assert f['_header'].attrs['modified']
+    assert f['packets']
+
+def test_to_file_v1_0_data_packet(tmpfile, data_packet):
+    to_file(tmpfile, [data_packet], version='1.0')
+    f = h5py.File(tmpfile, 'r')
+    assert len(f['packets']) == 1
+    row = f['packets'][0]
+    props = dtype_property_index_lookup['1.0']['packets']
+    new_packet = Packet()
+    new_packet.chip_key = row[props['chip_key']]
+    new_packet.packet_type = row[props['type']]
+    new_packet.chipid = row[props['chipid']]
+    new_packet.parity_bit_value = row[props['parity']]
+    new_packet.channel = row[props['channel']]
+    new_packet.timestamp = row[props['timestamp']]
+    new_packet.dataword = row[props['adc_counts']]
+    new_packet.fifo_half_flag = row[props['fifo_half']]
+    new_packet.fifo_full_flag = row[props['fifo_full']]
+    new_packet.direction = row[props['direction']]
+    assert new_packet == data_packet
+
+def test_to_file_v1_0_config_read_packet(tmpfile, config_read_packet):
+    to_file(tmpfile, [config_read_packet], version='1.0')
+    f = h5py.File(tmpfile, 'r')
+    assert len(f['packets']) == 1
+    row = f['packets'][0]
+    props = dtype_property_index_lookup['1.0']['packets']
+    new_packet = Packet()
+    new_packet.chip_key = row[props['chip_key']]
+    new_packet.packet_type = row[props['type']]
+    new_packet.chipid = row[props['chipid']]
+    new_packet.parity_bit_value = row[props['parity']]
+    new_packet.register_address = row[props['register']]
+    new_packet.register_data = row[props['value']]
+    new_packet.direction = row[props['direction']]
+    assert new_packet == config_read_packet
+
+def test_to_file_v1_0_timestamp_packet(tmpfile, timestamp_packet):
+    to_file(tmpfile, [timestamp_packet], version='1.0')
+    f = h5py.File(tmpfile, 'r')
+    assert len(f['packets']) == 1
+    row = f['packets'][0]
+    props = dtype_property_index_lookup['1.0']['packets']
+    assert row[1] == 4  # packet_type
+    new_packet = TimestampPacket()
+    new_packet.timestamp = row[props['timestamp']]
+    assert new_packet == timestamp_packet
+
+def test_to_file_v1_0_many_packets(tmpfile, data_packet, config_read_packet,
+        timestamp_packet):
+    to_file(tmpfile, [data_packet, config_read_packet,
+        timestamp_packet], version='1.0')
+    f = h5py.File(tmpfile, 'r')
+    assert len(f['packets']) == 3
+
+def test_from_file_v1_0_many_packets(tmpfile, data_packet,
+        config_read_packet, timestamp_packet):
+    packets = [data_packet, config_read_packet, timestamp_packet]
+    to_file(tmpfile, packets, version='1.0')
     new_packets_dict = from_file(tmpfile)
     assert new_packets_dict['created']
     assert new_packets_dict['version']
@@ -106,3 +189,9 @@ def test_from_file_many_packets(tmpfile, data_packet,
     assert new_packets[0] == data_packet
     assert new_packets[1] == config_read_packet
     assert new_packets[2] == timestamp_packet
+
+def test_from_file_incompatible(tmpfile):
+    to_file(tmpfile, [], version='0.0')
+    with pytest.raises(RuntimeError):
+        from_file(tmpfile, version='1.0')
+        pytest.fail('Should identify incompatible version')
