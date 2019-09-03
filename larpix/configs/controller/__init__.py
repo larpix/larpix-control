330397717a1863d73c87327c0db13c888a5742aa
Sam Kohn <kohn@berkeley.edu>
2019-09-03 15:21:33 -0700

    Add "layout" version to configs/controller files
    The layout version specifies which PCB layout is used, and is compatible
    with the layouts in LArPix Geometry

--- larpix/configs/controller/__init__.py
+++ larpix/configs/controller/__init__.py
@@ -4,10 +4,15 @@ The format is a standard JSON file structured as follows:
 
 .. parsed-literal::
     {
-        "name": <string identifier for system (typically pcb-<int> for a single pixel tile)>,
-        "chip_list": [<A list of chip keys, one for each chip>]
+        "name": <string identifier for PCB ID, e.g. "pcb-3">,
+        "layout": <string identifier for layout version, e.g. "1.2.0">,
+        "chip_list": [<A list of chip keys, one for each chip, in daisy-chain order>]
     }
 
 All fields are necessary.
+
+See the larpix-geometry documentation
+<https://github.com/samkohn/larpix-geometry> for a listing of all the
+layout versions.
 '''
 pass
