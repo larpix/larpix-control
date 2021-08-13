ca845db3c29f9418699d0d775670a6a2eb69dc11
Drinking Kazu <kterao@slac.stanford.edu>
2021-08-13 13:52:13 -0700

    relax the requirement constraining (#244)
    can be used w/ pyzmq > v18

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup(
             'pyserial ~=3.4',
             'pytest ~=6.2',
             'bitarray ~=0.8',
-            'pyzmq ~= 18.0',
+            'pyzmq >= 18.0',
             'sphinx_rtd_theme ~= 0.5',
             'numpy ~= 1.16',
             'h5py ~= 3.1',
